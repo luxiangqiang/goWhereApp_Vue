d06f63194ba478367479cdac1e67533606077e07
xiaolu <1040500646@qq.com>
2019-07-21 09:26:48 +0800

    城市搜索框

--- src/pages/City/City.vue
+++ src/pages/City/City.vue
@@ -1,16 +1,18 @@
 <template>  
     <div>
         <CityHead></CityHead>
+        <CitySearch></CitySearch>
     </div>
 </template>
 
-
 <script>
 import CityHead from '@/pages/City/components/Header'
+import CitySearch from '@/pages/City/components/Search'
 export default {
     name: 'city',
     components: {
         CityHead,
+        CitySearch
     }
 }
 </script>
---
+++ src/pages/City/components/Search.vue
@@ -0,0 +1,30 @@
+<template>  
+   <div class="search">
+       <input class="search-input" type="text" placeholder="输入城市名或拼音"/>
+   </div>
+</template>
+
+<script>
+export default {
+    name: 'CitySearch'
+}
+</script>
+
+<style>
+    .search{
+        height:.72rem;
+        background: #00bcd4;
+        padding: 0.1rem;
+    }
+
+    .search-input{
+        box-sizing: border-box;
+        width: 100%;
+        height: .62rem;
+        padding: 0.1rem;
+        line-height: .62rem;
+        text-align: center;
+        border-radius: .06rem;
+        color: #666;
+    }
+</style>
